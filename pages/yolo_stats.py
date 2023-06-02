9aef10ac8cae4cbcf5ce857f78ad51a55a0357c3
kef <kekekefir@gmail.com>
2023-06-02 16:10:35 +0300

    stats

--- pages/yolo_stats.py
+++ pages/yolo_stats.py
@@ -1,23 +1,62 @@
 import streamlit as st
 from PIL import Image
 def main():
+    
     results1 = Image.open('yolometrics/results1.png')
+    
     results2 = Image.open('yolometrics/results2.png')
+    
     results3 = Image.open('yolometrics/results3.png')
+    
     results4 = Image.open('yolometrics/results4.png')
 
+    
+    
     prcurve1 = Image.open('yolometrics/PR_curve1.png')
+    
     prcurve2 = Image.open('yolometrics/PR_curve2.png')
+    
     prcurve3 = Image.open('yolometrics/PR_curve3.png')
+    
     prcurve4 = Image.open('yolometrics/PR_curve4.png')
 
+    st.write('''
+    # Losses 
+    ''')
+    st.write('''
+    ### 25 epochs 
+    ''')
     st.image(results1, caption='25 epochs')
+    st.write('''
+    ### 50 epochs 
+    ''')
     st.image(results2, caption='50 epochs')
+    st.write('''
+    ### 100 epochs 
+    ''')
     st.image(results3, caption='100 epochs')
+    st.write('''
+    ### 200 epochs 
+    ''')
     st.image(results4, caption='200 epochs')
+    st.write('''
+    # Precision-Recall Curve 
+    ''')
+    st.write('''
+    ### 25 epochs 
+    ''')
     st.image(prcurve1, caption='25 epochs')
+    st.write('''
+    ### 50 epochs 
+    ''')
     st.image(prcurve2, caption='50 epochs')
+    st.write('''
+    ### 100 epochs 
+    ''')
     st.image(prcurve3, caption='100 epochs')
+    st.write('''
+    ### 200 epochs 
+    ''')
     st.image(prcurve4, caption='200 epochs')
 if __name__ == '__main__':
     main()
