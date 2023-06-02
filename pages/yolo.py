e40a99216e1a1594a6057239c90cb702362c2609
kef <kekekefir@gmail.com>
2023-06-02 15:34:13 +0300

    yolooo

--- pages/yolo.py
+++ pages/yolo.py
@@ -10,14 +10,25 @@ import numpy as np
 def main():
     st.title("Yolo turbines Detector")
     st.write("Let's find your turbines using computer vision") 
-    model = YOLO("8n_25epochs.pt")
+    option = st.selectbox(
+    'How many epochs should I train?',
+    ('25', '50', '100', '200'))
+    
+    if option == '25':
+        model = YOLO("8n_25epochs.pt")
+    elif option == '50':
+        model = YOLO("8n_50epochs.pt")
+    elif option == '100':
+        model = YOLO("8n_100epochs.pt")
+    else:
+        model = YOLO("8n_200epochs.pt")
+
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         
         image = Image.open(uploaded_file)
-        st.image(image)
         results = model.predict(source=image)
         st.image(results[0].plot())
 
